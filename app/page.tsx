3cda0217961fb9f8c01f78f624c5d57a02720df1
Matthew Bratrsovsky <git.twisted254@slmail.me>
2024-04-20 11:19:46 -0600

    feat: basic layout and content

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,126 @@
-import Image from "next/image";
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
+  const params = useParams();
+  const [hash, setHash] = useState("");
+
+  useEffect(() => {
+    const currentHash = window.location.hash.replace("#", "");
+    setHash(currentHash);
+  }, [params]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen lg:flex-row flex-col relative bg-surface-50 overflow-x-clip">
+      <nav className="contents">
+        <ul className="w-screen lg:w-max h-fit lg:h-screen bg-secondary-600 text-black flex lg:flex-col flex-col sm:flex-row justify-center sm:sticky left-0 top-0 items-end uppercase lg:pl-4 text-xl gap-x-8">
+          <li
+            className={clsx("w-max py-3 px-4 flex-1 lg:flex-none text-center", {
+              "border-primary-50 font-bold border-b-2 lg:border-b-0 lg:border-r-2":
+                hash === "home",
+            })}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <Link href="#home" >
+              Home
+            </Link>
+          </li>
+          <li
+            className={clsx("w-max py-3 px-4 flex-1 text-center lg:flex-none", {
+              "border-primary-50 font-bold border-b-2 lg:border-b-0 lg:border-r-2":
+                hash === "contact",
+            })}
+          >
+            <Link href="#contact" >
+              Contact
+            </Link>
+          </li>
+          <li className={clsx("w-max py-3 px-4 flex-1 text-center  lg:flex-none ")}>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSf3zQ7v5da7NYgnnPBfGzkIGbLJZdwnA5YbInfdDgm3zNWOYQ/viewform" target="_blank" rel="noreferrer" className="flex flex-row gap-x-2" >
+              <span>
+                Credit App
+              </span>
+              <span className="text-xs self-start flex">
+                [_↗]
+              </span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex flex-col w-full text-white">
+        <section className="bg-primary-800/85  h-screen gap-y-4 w-full justify-center flex flex-col px-10">
+          <div className="gap-y-2 flex flex-col">
+            <h1 className="text-5xl font-black">CMB AUTO SALES</h1>
+            <p className="font-bold text-xl">Family Owned Since 1946</p>
+          </div>
+          <a
+            className="rounded-full p-4 bg-primary-200 w-max font-bold shadow-primary-50 shadow-sm hover:bg-primary-300 outline-primary-50 outline-4 transition-colors "
+            href="https://docs.google.com/forms/d/e/1FAIpQLSf3zQ7v5da7NYgnnPBfGzkIGbLJZdwnA5YbInfdDgm3zNWOYQ/viewform"
+            target="_blank"
+          >
+            <span className="text-surface-900 uppercase flex flex-row gap-2">
+              <span>
+                Submit Credit Application
+              </span>
+              <span className="text-xs self-start flex">
+                [_↗]
+              </span>
+            </span>
           </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        </section>
+        <section
+          id="contact"
+          className="bg-secondary-800/85 min-h-24 py-10 space-y-4  w-full flex flex-row flex-wrap px-4 lg:px-10 gap-x-4 justify-around"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="flex-initial w-full border-b-2 pb-4 border-surface-400/25 ">
+            <h2 className="text-xl underline">Contact</h2>
+            <p>Send us an email or drop on by.</p>
+          </div>
+          <section className="flex flex-col sm:flex-none sm:grid gap-x-4 gap-y-2 grid-cols-[auto,_1fr] flex-1 sm:min-w-80" id="hours">
+            <h3 className="col-span-full font-black underline uppercase text-lg ">
+              Hours
+            </h3>
+            <span className="font-bold">Monday - Friday</span>
+            <span>8:00 AM - 5:00 PM</span>
+            <span className="font-bold">Saturday</span>
+            <span>8:00 AM - 12:00 PM</span>
+            <span className="font-bold">Sundays, Holy Days</span>
+            <span>Closed</span>
+          </section>
+          <section className="flex-none min-w-32">
+            <h3 className="col-span-full font-black underline uppercase text-lg">
+              Address
+            </h3>
+            <p>
+              16333 County Road V<br />
+              Fort Morgan, CO 80701
+            </p>
+          </section>
+          <section className="flex-none">
+            <h3 className="col-span-full font-black underline uppercase text-lg">
+              Phone
+            </h3>
+            <a href="tel:+9705676156">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              (970) 867-6156
+            </a>
+          </section>
+        </section>
+        <footer className="bg-primary-800 h-max text-sm py-4 w-full justify-around flex flex-col md:flex-row px-10 gap-y-2">
+          <p>© CMB AUTO SALES</p>
+          <a href="https://mattkbrat.com" target="_blank" rel="noreferrer" className="flex flex-row gap-2 text-xs">
+            <span>
+              Designed and Created by
+              Matthew Bratrsovsky
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+            <span className="text-xs self-start flex">
+              [_↗]
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          </a>
+        </footer>
       </div>
     </main>
   );
