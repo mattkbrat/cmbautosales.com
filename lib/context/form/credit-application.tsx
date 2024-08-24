0c20118ee908f8e0f6dc7f0dee92661860d2cdb9
Matthew Bratrsovsky <realzing@pm.me>
2024-08-24 12:34:03 -0600

    feat(credit-app): context

---
+++ lib/context/form/credit-application.tsx
@@ -0,0 +1,284 @@
+import { CompleteFormSection, Introduction } from "@/components";
+import type { ArrayElement, InputMapFieldSection, InputMap } from "@/types";
+import {
+	proofs,
+	personal,
+	employment,
+	housing,
+	renting,
+	reference,
+} from "./sections";
+
+const sections = [
+	"introduction",
+	"applicable_form",
+	"personal",
+	"pictures",
+	"employment",
+	"housingOrRenting",
+	"housing",
+	"renting",
+	"complete",
+	"reference_1",
+	"reference_2",
+	"reference_3",
+	"reference_4",
+	"reference_5",
+	"reference_6",
+	"submit",
+] as const;
+
+type Section = ArrayElement<typeof sections>;
+
+export const APPLICATION_STATES: {
+	[key in Uppercase<Section>]: {
+		hash: Section;
+		introduction?: string;
+		title?: string;
+		next?: Section | null;
+		component?: () => React.ReactNode;
+	};
+} = {
+	INTRODUCTION: {
+		hash: "introduction",
+		introduction: "",
+		title: "Introduction",
+		component: () => <Introduction />,
+	},
+	APPLICABLE_FORM: {
+		hash: "applicable_form",
+		introduction: "This form is split into several sections.",
+		component: () => (
+			<>
+				<section className="flex flex-row gap-2  flex-1">
+					<h3>Which form are you filling out?</h3>
+				</section>
+				<p>You can come back later to fill out a different half.</p>
+			</>
+		),
+		title: "Applicable Form",
+	},
+	PERSONAL: {
+		hash: "personal",
+		introduction: "Applicant Info",
+		title: "Personal Information",
+	},
+	EMPLOYMENT: {
+		hash: "employment",
+		introduction: "",
+		title: "Employment",
+	},
+	HOUSINGORRENTING: {
+		hash: "housingOrRenting",
+		introduction: "",
+		title: "Housing or Renting",
+	},
+	HOUSING: {
+		hash: "housing",
+		introduction: "Only complete if buying / own",
+		title: "Housing information",
+		next: "complete",
+	},
+	RENTING: {
+		hash: "renting",
+		introduction: "Only complete if renting",
+		title: "Renting information",
+		next: "complete",
+	},
+	COMPLETE: {
+		hash: "complete",
+		introduction: "",
+		title: "",
+		component: () => <CompleteFormSection />,
+		next: "submit",
+	},
+	REFERENCE_1: {
+		hash: "reference_1",
+		introduction: "Please list at least 4.",
+		title: "Reference #1",
+	},
+	REFERENCE_2: {
+		hash: "reference_2",
+		introduction: "",
+		title: "Reference #2",
+	},
+	REFERENCE_3: {
+		hash: "reference_3",
+		introduction: "",
+		title: "Reference #3",
+	},
+	REFERENCE_4: {
+		hash: "reference_4",
+		introduction: "",
+		title: "Reference #4",
+	},
+	REFERENCE_5: {
+		hash: "reference_5",
+		introduction: "",
+		title: "Reference #5",
+	},
+	REFERENCE_6: {
+		hash: "reference_6",
+		introduction: "",
+		title: "Reference #6",
+		next: "complete",
+	},
+	SUBMIT: {
+		hash: "submit",
+		introduction: "",
+		title: "",
+		next: null,
+	},
+	PICTURES: {
+		hash: "pictures",
+		introduction: "",
+		title: "Proofs",
+		next: "complete",
+	},
+} as const;
+
+export const applicationStates = Object.keys(APPLICATION_STATES);
+export const applicationRouteData = Object.values(APPLICATION_STATES);
+
+export type ApplicationState = keyof typeof APPLICATION_STATES;
+export type ApplicationHash =
+	(typeof APPLICATION_STATES)[ApplicationState]["hash"];
+
+// const references = (number: number): InputMapFieldSection[] => [
+// 	{
+// 		key: `reference #${number}`,
+// 		fields: [
+// 			{
+// 				key: `name-${number}`,
+// 				text: "Name",
+// 				name: "name",
+// 				optional: number > 4 ? true : undefined,
+// 			},
+// 			{
+// 				key: `Street-${number}`,
+// 				text: "street name",
+// 				name: "street",
+// 				optional: number > 4 ? true : undefined,
+// 			},
+// 			{
+// 				key: `Number-${number}`,
+// 				text: "number",
+// 				name: "number",
+// 				optional: true,
+// 			},
+// 			{
+// 				key: `floor-${number}`,
+// 				text: "Floor, unit...",
+// 				name: "floor",
+// 				optional: true,
+// 			},
+// 			{
+// 				key: `city-${number}`,
+// 				text: "City",
+// 				name: "city",
+// 				optional: number > 4 ? true : undefined,
+// 			},
+// 			{
+// 				key: `state-${number}`,
+// 				text: "State",
+// 				name: "state",
+// 				optional: number > 4 ? true : undefined,
+// 			},
+// 			{
+// 				key: `zip-${number}`,
+// 				text: "ZIP",
+// 				name: "zip",
+// 				optional: number > 4 ? true : undefined,
+// 			},
+// 			{
+// 				key: `phone-${number}`,
+// 				text: "Phone #",
+// 				name: "phone",
+// 				type: "tel",
+// 				optional: number > 4 ? true : undefined,
+// 			},
+// 			{
+// 				key: `phone2-${number}`,
+// 				text: "Phone # (work)",
+// 				name: "phone2",
+// 				optional: true,
+// 			},
+// 		],
+// 	},
+// ];
+
+export const inputs: InputMap<ApplicationState> = {
+	PERSONAL: personal,
+	INTRODUCTION: [
+		{
+			key: "confirm",
+			fields: [
+				{
+					key: "confirm",
+					text: "I have all of this information",
+					type: "checkbox",
+				},
+			],
+		},
+	],
+	APPLICABLE_FORM: [
+		{
+			key: "Select a Form",
+			fields: [
+				{
+					type: "radio",
+					text: "",
+					options: [
+						{
+							key: APPLICATION_STATES.PERSONAL.hash,
+							value: "Start with Credit Application",
+						},
+						{
+							key: APPLICATION_STATES.REFERENCE_1.hash,
+							value: "Skip to References",
+						},
+						{
+							key: APPLICATION_STATES.PICTURES.hash,
+							value: "Submit image proofs",
+						},
+					],
+					key: "formSelection",
+				},
+			],
+		},
+	],
+	PICTURES: proofs,
+	HOUSINGORRENTING: [
+		{
+			key: "Housing Or Renting",
+			fields: [
+				{
+					type: "radio",
+					text: "",
+					options: [
+						{
+							key: APPLICATION_STATES.HOUSING.hash,
+							value: "Housing",
+						},
+						{
+							key: APPLICATION_STATES.RENTING.hash,
+							value: "Renting",
+						},
+					],
+					key: "housingOrRenting",
+				},
+			],
+		},
+	],
+	EMPLOYMENT: employment,
+	REFERENCE_1: reference<1>(1),
+	REFERENCE_2: reference<2>(2),
+	REFERENCE_3: reference<3>(3),
+	REFERENCE_4: reference<4>(4),
+	REFERENCE_5: reference<5>(5),
+	REFERENCE_6: reference<6>(6),
+	SUBMIT: [],
+	HOUSING: housing,
+	RENTING: renting,
+	COMPLETE: [],
+} as const;
---
+++ lib/context/form/form-context.tsx
@@ -0,0 +1,222 @@
+"use client";
+import {
+	createContext,
+	useContext,
+	useEffect,
+	useMemo,
+	useReducer,
+	useState,
+	type Dispatch,
+	type ReactNode,
+} from "react";
+
+import rss from "react-secure-storage";
+import { APPLICATION_STATES, type ApplicationHash } from "./credit-application";
+import type { FormKey } from "./sections";
+import { ArrayElement } from "@/types";
+
+type FormImageAction =
+	| {
+			type: "set";
+			image: File;
+	  }
+	| {
+			type: "remove";
+			index: number;
+	  }
+	| {
+			type: "reset";
+	  }
+	| {
+			type: "submit";
+	  };
+export type FormContextType = {
+	section: string;
+	state: FormData;
+	dispatch: Dispatch<FormInputAction>;
+	clearForm: () => void;
+	loaded: boolean;
+	breadcrumbs: string[];
+	images: {
+		current: File[];
+		dispatch: Dispatch<FormImageAction>;
+	};
+};
+
+const Context = createContext<FormContextType>({
+	section: "",
+	state: {},
+	loaded: false,
+	dispatch: () => {},
+	clearForm: () => {},
+	breadcrumbs: [],
+	images: {
+		current: [],
+		dispatch: () => {},
+	},
+});
+
+export const useFormContext = () => useContext(Context);
+
+type FormData = Partial<{ [key in FormKey]: string | number | Date }>;
+
+type FormInputAction =
+	| {
+			type: "set";
+			key: keyof FormData;
+			value: unknown;
+	  }
+	| {
+			type: "reset";
+	  };
+
+function formReducer(state: FormData, action: FormInputAction): FormData {
+	if (action.type === "set") {
+		return {
+			...state,
+			[action.key]: action.value || undefined,
+		} as FormData;
+	}
+	if (action.type === "reset") {
+		return {};
+	}
+
+	return state;
+}
+
+function imageReducer(images: File[], action: FormImageAction) {
+	if (action.type === "set") {
+		const currentIndex = images.findIndex((i) => i.name === action.image.name);
+		if (currentIndex !== -1) {
+			images[currentIndex] = action.image;
+		} else {
+			images.push(action.image);
+		}
+
+		return images;
+	}
+	if (action.type === "remove") {
+		images.splice(action.index, 1);
+		return images;
+	}
+	if (action.type === "reset") {
+		return [];
+	}
+
+	if (action.type === "submit") {
+		console.log("saving images", images);
+		return images;
+	}
+
+	return images;
+}
+
+export const FormProvider = (p: {
+	children: ReactNode;
+	form: string;
+}) => {
+	const [loaded, setLoaded] = useState(false);
+
+	const keys = useMemo(() => {
+		return {
+			section: `${p.form}-section`,
+			data: `${p.form}-data`,
+			breadcrumbs: `${p.form}-nav`,
+		};
+	}, [p.form]);
+
+	const lastSection = useMemo(() => {
+		const set = rss.getItem(keys.section);
+		if (typeof set === "string") return set;
+		return APPLICATION_STATES.INTRODUCTION.hash;
+	}, [keys.section]);
+
+	const [formState, dispatchFormState] = useReducer(formReducer, {});
+	const [images, dispatchImages] = useReducer(imageReducer, []);
+
+	useEffect(() => console.log("up", images), [images]);
+
+	const section = useMemo(() => {
+		return typeof formState.section === "string"
+			? formState.section
+			: lastSection || "";
+	}, [formState.section, lastSection]);
+
+	const breadcrumbs: ApplicationHash[] = useMemo(() => {
+		const stateSet = formState.breadcrumbs;
+		if (Array.isArray(stateSet)) return stateSet;
+		const set = rss.getItem(keys.breadcrumbs);
+		if (Array.isArray(set)) return set;
+		return [APPLICATION_STATES.INTRODUCTION.hash];
+	}, [keys.breadcrumbs, formState.breadcrumbs]);
+
+	useEffect(() => {
+		const sectionTyped = section as ApplicationHash;
+		const sectionIndex = breadcrumbs.indexOf(sectionTyped);
+		const newBreadcrumbs = [...breadcrumbs];
+		if (sectionIndex !== -1) {
+			console.log("splicing", newBreadcrumbs[sectionIndex], sectionTyped);
+			newBreadcrumbs.splice(sectionIndex, 1);
+		}
+		newBreadcrumbs.push(sectionTyped);
+		if (newBreadcrumbs.slice(-1)[0] === breadcrumbs.slice(-1)[0]) {
+			console.log("matches", { newBreadcrumbs, breadcrumbs });
+			return;
+		}
+		console.log("up bread", breadcrumbs);
+		dispatchFormState({
+			type: "set",
+			value: newBreadcrumbs,
+			key: "breadcrumbs",
+		});
+	}, [section, breadcrumbs]);
+
+	useEffect(() => {
+		const exists = rss.getItem(keys.data);
+		if (!exists || !(exists instanceof Object)) {
+			setLoaded(true);
+			return;
+		}
+
+		for (const [k, v] of Object.entries(exists)) {
+			dispatchFormState({ type: "set", key: k as keyof FormData, value: v });
+		}
+
+		setTimeout(() => {
+			setLoaded(true);
+		}, 200);
+	}, [keys.data]);
+
+	useEffect(() => {
+		if (!loaded) return;
+		rss.setItem(keys.data, formState);
+	}, [formState, loaded, keys.data]);
+
+	const clearForm = () => {
+		console.log("clearing", formState, section);
+		rss.setItem(keys.data, {});
+		rss.setItem(keys.section, "");
+		rss.setItem(keys.breadcrumbs, [APPLICATION_STATES.INTRODUCTION.hash]);
+		dispatchFormState({ type: "reset" });
+		dispatchImages({ type: "reset" });
+	};
+
+	return (
+		<Context.Provider
+			value={{
+				section,
+				state: formState,
+				dispatch: dispatchFormState,
+				clearForm,
+				loaded,
+				breadcrumbs,
+				images: {
+					current: images,
+					dispatch: dispatchImages,
+				},
+			}}
+		>
+			{p.children}
+		</Context.Provider>
+	);
+};
