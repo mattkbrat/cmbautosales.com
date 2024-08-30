48ebd191724a47fcf90ccc0ee16d0d9dc99d7630
Matthew Bratrsovsky <realzing@pm.me>
2024-08-30 12:04:50 -0600

    fix(auth): middleware seems to be broken for github provider
    Moved auth checks to layouts

--- app/(protected)/credit-application/layout.tsx
+++ app/(protected)/credit-application/layout.tsx
@@ -1,16 +1,26 @@
+import { UserBar } from "@/components";
+import { getServerSession } from "@/lib/auth";
 import { FormProvider } from "@/lib/context";
+import { redirect } from "next/navigation";
 
 const CreditApplicationLayout = async ({
 	children,
 }: { children: React.ReactNode }) => {
+	const session = await getServerSession();
+
+	if (!session?.user) {
+		return redirect("/auth/signin?returnTo=/credit-application");
+	}
 	return (
 		<div
 			className="mx-[10dvw] flex flex-col mt-4 border-t-[0.4rem] rounded-lg flex-1
 		  "
 		>
-			<div className="gap-4 flex flex-col form flex-1">
-				<section className="bg-darkblue-transparent">
+			<div className="gap-4 flex flex-col form flex-1 ">
+				<section className="bg-darkblue-transparent px-4 py-2 space-y-2">
 					<h1 className="text-2xl">CMB Auto Sales - Credit Application</h1>
+					<hr />
+					<UserBar intro="Submitting as" user={session.user} />
 				</section>
 				<div className="flex flex-col flex-1">
 					<FormProvider form={"credit"}>{children}</FormProvider>
