542ec2ab26eef830c098560b3a7bc0da6b7c0cc2
Matthew Bratrsovsky <realzing@pm.me>
2024-08-30 12:33:06 -0600

    fix(auth): improve styling

--- app/auth/signin/SignInView.tsx
+++ app/auth/signin/SignInView.tsx
@@ -2,19 +2,34 @@
 
 import type { AuthProviders } from "@/types";
 import { signIn } from "next-auth/react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 export const SignInView = ({
 	providers,
 }: { providers: NonNullable<AuthProviders> }) => {
+	const icons = {
+		Google: <FaGoogle />,
+		GitHub: <FaGithub />,
+	};
+
 	return (
-		<>
+		<ul className="flex flex-col text-4xl self-center my-auto gap-4 bg-black/80 py-10 px-10 rounded-md">
 			{Object.values(providers).map((provider) => (
-				<div key={provider.name}>
-					<button type="button" onClick={() => signIn(provider.id)}>
-						Sign in with {provider.name}
+				<li key={provider.name}>
+					<button
+						type="button"
+						onClick={() => signIn(provider.id)}
+						className="flex flex-row gap-x-2 "
+					>
+						<span>
+							{provider.name in icons &&
+								icons[provider.name as keyof typeof icons]}
+						</span>
+						<span>Sign in with</span>
+						<span>{provider.name}</span>
 					</button>
-				</div>
+				</li>
 			))}
-		</>
+		</ul>
 	);
 };
