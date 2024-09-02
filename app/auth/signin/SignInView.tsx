2df77597a9ecd5ab178fb70746fe416443dfcac7
Matthew Bratrsovsky <realzing@pm.me>
2024-09-02 08:49:25 -0600

    fix(style): awkward auth provider wrapping on mobile
    resolves #4

--- app/auth/signin/SignInView.tsx
+++ app/auth/signin/SignInView.tsx
@@ -7,30 +7,39 @@ import { FaGithub, FaGoogle } from "react-icons/fa";
 export const SignInView = ({
 	providers,
 }: { providers: NonNullable<Provider[]> }) => {
-	const icons = {
-		Google: (color?: string) => <FaGoogle color={color || "white"} />,
-		GitHub: (color?: string) => <FaGithub color={color || "white"} />,
-	};
-
 	return (
-		<ul className="flex flex-col text-4xl self-center my-auto gap-4 bg-black/80 py-10 px-10 rounded-md">
+		<ul className="flex flex-col text-4xl items-center my-auto gap-4 bg-black/80 py-10 rounded-md lg:mx-[10dvw]">
+			<div className="uppercase">
+				<span className="">
+					<span>Sign In</span>
+					<span>&nbsp;</span>
+					<span className="lg:hidden">With</span>
+				</span>
+			</div>
+			<hr className="text-white w-3/4 lg:w-1/2" />
 			{Object.values(providers).map((provider) => (
-				<li key={provider.name}>
+				<li
+					key={provider.name}
+					className=""
+					style={{
+						backgroundColor: provider.style.bg || provider.style.brandColor,
+					}}
+				>
 					<button
 						type="button"
 						onClick={() => signIn(provider.id)}
-						className="flex flex-row gap-x-2 "
+						className="flex-1 py-4 px-10 flex gap-x-2 rounded-md shadow-sm justify-between"
 					>
+						{provider.name === "Google" ? (
+							<FaGoogle />
+						) : (
+							provider.name === "GitHub" && <FaGithub />
+						)}
 						<span>
-							{provider.name in icons &&
-								icons[provider.name as keyof typeof icons](
-									provider.style.logo ||
-										provider.style.brandColor ||
-										provider.style.bg,
-								)}
+							<span className="hidden lg:inline-block">Sign In With</span>
+							<span>&nbsp;</span>
+							<span>{provider.name}</span>
 						</span>
-						<span>Sign in with</span>
-						<span>{provider.name}</span>
 					</button>
 				</li>
 			))}
